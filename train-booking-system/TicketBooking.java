b6b7ce0ee3fca9580b79ca008087c775d939b1cf
NareshAiNexus <nare95143@gmail.com>
2025-06-06 23:02:42 +0530

    commit

--- train-booking-system/TicketBooking.java
+++ train-booking-system/TicketBooking.java
@@ -1,5 +1,11 @@
+import java.util.*;
+
 public class TicketBooking {
   public static void main(String[] args) {
+    
+    TicketSystem ticketSystem = new TicketSystem();
+    Scanner scan = new Scanner(System.in);
+
     
   }
 }
