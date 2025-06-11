025f124d58ce3db87d4cb71cb14b428c4c135ac1
NareshAiNexus <nare95143@gmail.com>
2025-06-11 21:45:29 +0530

    Flight Reservation class was created

---
+++ flight-booking-system/FlightReservation.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+class FlightReservation {
+
+  Map<String, Flight> flights;
+
+  public FlightReservation(){
+    flights = new HashMap<>();
+    flights.put("Indigo" , new Flight("Indigo"));
+    flights.put("SpiceJet" , new Flight("SpiceJet"));
+
+  }
+
+  public String bookTicket()
+
+}
